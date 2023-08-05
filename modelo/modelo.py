ffa73353ef1ce68cb8b6a2999452642c0e9d5b16
christian180492 <christianojedabaruja@gmail.com>
2023-08-04 20:29:31 -0400

    Codificación de modelo.py para predecir categorias.
    Toda la api ya es funcional, sin embargo al ejecutar un ejemplo, me he percatado que el modelo que se importa en el contenedor de modelos, hay ocasiones que retorna vetores con puros ceros, por lo que debo encarar un reentrenamiento teniendo en cuenta tensor flow.

--- modelo/modelo.py
+++ modelo/modelo.py
@@ -1,12 +1,74 @@
 import redis
 import pickle
+import nltk
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.multioutput import MultiOutputClassifier
 from sklearn.model_selection import RandomizedSearchCV
 
+import pandas as pd
+
+y = pd.read_csv('ejemplo_salida.csv')
+
+from src import text_normalizer
+stop_words = nltk.corpus.stopwords.words("english")
+
+from nltk.tokenize.toktok import ToktokTokenizer
+
+import gensim.downloader
+pretrained_w2v_model = gensim.downloader.load('glove-wiki-gigaword-300')
+
 # Cargar el modelo al iniciar el script
 ruta_modelo = '/app/modelos/best_moc.pkl'  # Ruta del modelo dentro del contenedor
 modelo = None
+
+def normalizacion(oracion):
+    oracion_normalizada = text_normalizer.normalize_corpus(oracion, stopwords=stop_words, text_lemmatization=True)
+    return oracion_normalizada
+
+def generate_ngrams(tokens, n):
+    if n == 1:
+        return tokens
+    else:
+        return [' '.join(ngram) for ngram in ngrams(tokens, n)]
+
+def toktok(oraciones, ninit=1, nfin=1):
+    '''
+    oraciones: La lista de oraciones que se desea tokenizar.
+    ninit: Cantidad de ngramas mínimo que uno desea obtener como tokens.
+    nfin: Cantidad de ngramas máximo que uno desea obtener como tokens.
+    '''
+
+    tokenizer = ToktokTokenizer()
+
+    oraciones_tokenizadas_ngramas = []
+    
+    for oracion in oraciones:
+        ngram_tokens = [generate_ngrams(tokenizer.tokenize(oracion), n) for n in range(ninit, nfin+1)]
+        ngram_tokens_flattened = [token for ngram in ngram_tokens for token in ngram]
+        oraciones_tokenizadas_ngramas.append(ngram_tokens_flattened)
+        
+    return oraciones_tokenizadas_ngramas
+
+def vectorizer_pretrained(corpus, model, num_features: int=100):
+    corpus_size = len(corpus)
+    corpus_vectors = np.zeros((corpus_size, num_features), dtype=np.float32)
+
+    for i, document in enumerate(corpus):
+        word_count = 0
+        doc_vector = np.zeros(num_features, dtype=np.float32)
+
+        for word in document:
+            if word in model:
+                doc_vector += model.word_vec(word)
+                word_count+=1
+        
+        if word_count > 0:
+            doc_vector /= word_count
+        
+        corpus_vectors[i] = doc_vector
+    
+    return corpus_vectors
 
 def cargar_modelo(ruta_modelo):
     global modelo  # Utiliza la variable global del modelo
@@ -21,8 +83,21 @@ def procesar_datos(nombre, descripcion, imagen):
         cargar_modelo(ruta_modelo)
 
     # Lógica para procesar los datos recibidos y obtener las categorías usando el modelo
-    # Ejemplo de uso del modelo:
-    # categorias = modelo.predict([datos_a_procesar])
+    descripcion_normalizada = normalizacion([descripcion])
+    print(f'\n\nESTA ES LA DESCRIPCIÓN NORMALIZADA {descripcion_normalizada}\n\n')
+    descripcion_tokenizada = toktok(descripcion_normalizada, 1, 1)
+    print(f'\n\nESTA ES LA DESCRIPCIÓN TOKENIZADA {descripcion_tokenizada}\n\n')
+    descripcion_vectorizada = vectorizer_pretrained(descripcion_tokenizada, pretrained_w2v_model, 300)
+    categoria_binaria = modelo.predict(descripcion_vectorizada)
+    print(categoria_binaria)
+
+    clave_valor = zip(y.columns, categoria_binaria[0])
+    diccionario = dict(clave_valor)
+
+    categorias = []
+    for clave, valor in diccionario.items():
+        if valor != 0:
+            categorias.append(clave)
 
     categorias = ['Categoria 1', 'Categoria 2', 'Categoria 3']  # Ejemplo: categorías predichas por el modelo
     return categorias
